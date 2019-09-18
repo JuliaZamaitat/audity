7f63a31eeda13f84cf97fe1a61b6eb914ee0ea64
Julia Zamaitat <j.zamaitat@gmail.com>
2019-09-18 10:13:36 +0200

    Made modal segue for player present over whole screen.

--- Audiobooks/Audiobooks/PlayerViewController.swift
+++ Audiobooks/Audiobooks/PlayerViewController.swift
@@ -11,15 +11,13 @@ import UIKit
 class PlayerViewController: ViewControllerPannable {
 
    
-   
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
         // Do any additional setup after loading the view.
     }
     
-
     /*
     // MARK: - Navigation
 
