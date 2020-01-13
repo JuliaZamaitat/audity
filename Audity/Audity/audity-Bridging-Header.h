cd75e0984a5816f167c0f86fd4f0037abcedf50d
Julia Zamaitat <j.zamaitat@gmail.com>
2020-01-13 21:36:28 +0100

    Renamed project to Audity.

---
+++ Audity/Audity/audity-Bridging-Header.h
@@ -0,0 +1,15 @@
+//
+//  audity-Bridging-Header.h
+//  Audity
+//
+//  Created by Julia Zamaitat on 06.09.19.
+//  Copyright © 2019 Julia Zamaitat. All rights reserved.
+//
+
+
+#ifndef audity_Bridging_Header_h
+#define audity_Bridging_Header_h
+
+
+#import <SpotifyiOS/SpotifyiOS.h>
+#endif /* audity_Bridging_Header_h */
