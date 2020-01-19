1091ad15e89055319d95bfd6f5af151011bcd01d
Julia Zamaitat <j.zamaitat@gmail.com>
2020-01-19 20:09:49 +0100

    Used optional binding instead of force unwrapping.

--- Audity/Audity/DetailCoverViewController.swift
+++ Audity/Audity/DetailCoverViewController.swift
@@ -1,6 +1,6 @@
 //
 //  DetailCoverViewController.swift
-//  Audiobooks
+//  Audity
 //
 //  Created by Julia Zamaitat on 08.09.19.
 //  Copyright © 2019 Julia Zamaitat. All rights reserved.
@@ -9,7 +9,6 @@
 import UIKit
 
 class DetailCoverViewController: UIViewController {
-
    
     @IBOutlet weak var coverImage: UIImageView!
     @IBOutlet weak var titelLabel: UILabel!
@@ -24,11 +23,10 @@ class DetailCoverViewController: UIViewController {
         adjustStyle()
         guard let audiobook = audiobook else {return}
         let url = audiobook.image
-        let data = try? Data(contentsOf: url) //make sure your image in this url does exist, otherwise unwrap in a if let check / try-catch
+        let data = try? Data(contentsOf: url)
         coverImage.image = UIImage(data: data!)
         coverImage.contentMode = .scaleAspectFit
         titelLabel.text = audiobook.title
-        // Do any additional setup after loading the view.
     }
     
     override func viewWillAppear(_ animated: Bool) {
@@ -38,6 +36,7 @@ class DetailCoverViewController: UIViewController {
             addToLibraryButton.setTitle("Zur Bibliothek hinzufügen", for: .normal)
         }
     }
+    
     func adjustStyle() {
         view.backgroundColor = UIColor.SpotifyColor.Black
         subView.backgroundColor = UIColor.SpotifyColor.Black
@@ -45,29 +44,28 @@ class DetailCoverViewController: UIViewController {
         addToLibraryButton.layer.cornerRadius = 15
         addToLibraryButton.layer.borderColor = UIColor.white.cgColor
         titelLabel.adjustsFontSizeToFitWidth = true
-        
-        
     }
 
     @IBAction func addToLibraryButtonTapped(_ sender: Any) {
-        if MyLibrary.myBooks.contains(audiobook!) {
-            if let index = MyLibrary.myBooks.firstIndex(of: audiobook!){
-                MyLibrary.myBooks.remove(at: index)
-                let alert = UIAlertController(title: "Bye", message: "Hörbuch entfernt", preferredStyle: UIAlertController.Style.alert)
+        if let book = audiobook {
+            if MyLibrary.myBooks.contains(book) {
+                if let index = MyLibrary.myBooks.firstIndex(of: book){
+                    MyLibrary.myBooks.remove(at: index)
+                    let alert = UIAlertController(title: "Bye", message: "Hörbuch entfernt", preferredStyle: UIAlertController.Style.alert)
+                    alert.addAction(UIAlertAction(title: "Schließen", style: UIAlertAction.Style.default, handler: nil))
+                    self.present(alert, animated: true, completion: nil)
+                    addToLibraryButton.setTitle("Zur Bibliothek hinzufügen", for: .normal)
+                    MyLibrary.saveToFile(books: MyLibrary.myBooks)
+                }
+               
+            } else {
+                MyLibrary.myBooks.append(book)
+                let alert = UIAlertController(title: "Yeay", message: "Erfolgreich hinzugefügt", preferredStyle: UIAlertController.Style.alert)
                 alert.addAction(UIAlertAction(title: "Schließen", style: UIAlertAction.Style.default, handler: nil))
                 self.present(alert, animated: true, completion: nil)
-                addToLibraryButton.setTitle("Zur Bibliothek hinzufügen", for: .normal)
-                
+                addToLibraryButton.setTitle("Aus Bibliothek entfernen", for: .normal)
                 MyLibrary.saveToFile(books: MyLibrary.myBooks)
             }
-           
-        } else {
-            MyLibrary.myBooks.append(audiobook!)
-            let alert = UIAlertController(title: "Yeay", message: "Erfolgreich hinzugefügt", preferredStyle: UIAlertController.Style.alert)
-            alert.addAction(UIAlertAction(title: "Schließen", style: UIAlertAction.Style.default, handler: nil))
-            self.present(alert, animated: true, completion: nil)
-            addToLibraryButton.setTitle("Aus Bibliothek entfernen", for: .normal)
-            MyLibrary.saveToFile(books: MyLibrary.myBooks)
         }
     }
     
