2c211d76039b140cfb197049153a9ebe60e81cfd
Julia Zamaitat <j.zamaitat@gmail.com>
2020-01-19 20:11:55 +0100

    Cleanup.

--- Audity/Audity/Audiobook.swift
+++ Audity/Audity/Audiobook.swift
@@ -1,6 +1,6 @@
 //
-//  Audiobook.swift
-//  Audiobooks
+//  Audity.swift
+//  Audity
 //
 //  Created by Julia Zamaitat on 07.09.19.
 //  Copyright © 2019 Julia Zamaitat. All rights reserved.
@@ -8,7 +8,6 @@
 
 import Foundation
 import UIKit
-
 
 struct Audiobook: Equatable, Codable {
     
@@ -43,17 +42,7 @@ struct Audiobook: Equatable, Codable {
         self.trackList = trackList
         self.duration = duration
         self.uri = uri
-        //self.releaseDate = dateformat(date: releaseDate)
     }
-    
-    /*init(from decoder: Decoder) throws {
-        let valueContainer = try decoder.container(keyedBy: Keys.self)
-        self.title = try valueContainer.decode(String.self, forKey: Keys.title)
-        self.author = try valueContainer.decode(String.self, forKey: Keys.author)
-        self.image = try valueContainer.decode(String.self, forKey: Keys.image)
-        self.releaseDate = try valueContainer.decode(String.self, forKey: Keys.releaseDate)
-        self.trackList = [try valueContainer.decode(Track.self, forKey: Keys.trackList)]
-    }*/
     
     static func == (lhs: Audiobook, rhs: Audiobook) -> Bool {
         return lhs.title == rhs.title && lhs.author == rhs.author && lhs.releaseDate == rhs.releaseDate
@@ -71,17 +60,6 @@ struct Audiobook: Equatable, Codable {
         }
         duration = minutes
     }
-    
-
-    
-    /* Maybe useful later when working with the Spotify API
-     func dateformat(date: String) -> Date?{
-    let input = date
-    let formatter = DateFormatter()
-    formatter.dateFormat = "yyyy/mm/dd"
-    guard let date = formatter.date(from: input) else {return nil}
-    return date
-    }*/
 }
 
 
